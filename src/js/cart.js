f15aa6f265a29e52092be0e9fe942828b5d6e684
matthewj1561 <77714715+matthewj1561@users.noreply.github.com>
2022-01-25 09:45:10 -0700

    fixed empty cart html error

--- src/js/cart.js
+++ src/js/cart.js
@@ -5,8 +5,10 @@ function getLocalStorage(key) {
 function getCartContents() {
   let markup = "";
   const cartItems = getLocalStorage("so-cart");
-  const htmlItems = cartItems.map((item) => renderCartItem(item));
-  document.querySelector(".product-list").innerHTML = htmlItems.join("");
+  if (cartItems != null) {
+    const htmlItems = cartItems.map((item) => renderCartItem(item));
+    document.querySelector(".product-list").innerHTML = htmlItems.join("");
+  }
   // document.querySelector(".product-list").innerHTML = renderCartItem(cartItems);
 }
 
