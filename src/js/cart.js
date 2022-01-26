75cde13c380a48f19b95f5b517ec82f0d619174f
arrowalker3 <arrowalker3@gmail.com>
2022-01-25 17:55:02 -0700

    Remove items from cart that have matching id with x that got clicked on in cart

--- src/js/cart.js
+++ src/js/cart.js
@@ -2,25 +2,45 @@ function getLocalStorage(key) {
   return JSON.parse(localStorage.getItem(key));
 }
 
+function setLocalStorage(key, data) {
+  localStorage.setItem(key, JSON.stringify(data));
+}
+
 function getCartContents() {
   let markup = "";
   const cartItems = getLocalStorage("so-cart");
   if (cartItems != null) {
     const htmlItems = cartItems.map((item) => renderCartItem(item));
     document.querySelector(".product-list").innerHTML = htmlItems.join("");
+
+    // Add event listeners to .cart-card__remove
+    const removeElements = [...document.getElementsByClassName("cart-card__remove")];
+    removeElements.map(element => element.addEventListener('click', removeFromCart));
   }
   // document.querySelector(".product-list").innerHTML = renderCartItem(cartItems);
+}
+
+function removeFromCart(e) {
+  e.preventDefault();
+  const productId = e.target.getAttribute("data-id");
+  
+  const cartItems = getLocalStorage("so-cart");
+  const filteredList = cartItems.filter(item => item.Id !== productId);
+
+  setLocalStorage("so-cart", filteredList);
+  getCartContents();
 }
 
 function renderCartItem(item) {
   const newItem = `<li class="cart-card divider">
   <a href="#" class="cart-card__image">
+    <span class="cart-card__remove" data-id="${item.Id}">X</span>
     <img
       src="${item.Image}"
       alt="${item.Name}"
     />
   </a>
-  <a href="#">
+  <a href="#" class="cart-card__name">
     <h2 class="card__name">${item.Name}</h2>
   </a>
   <p class="cart-card__color">${item.Colors[0].ColorName}</p>
