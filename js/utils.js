a3aff3fb92837a82741a97bd7a829b4143517083
arrowalker3 <arrowalker3@gmail.com>
2022-01-28 11:06:23 -0700

    Updates

--- js/utils.js
+++ js/utils.js
@@ -1,15 +1,44 @@
-export function qs(e, t = document) {
-  return t.querySelector(e);
+// wrapper for querySelector...returns matching element
+export function qs(selector, parent = document) {
+  return parent.querySelector(selector);
 }
-export function getLocalStorage(e) {
-  return JSON.parse(localStorage.getItem(e));
+// or a more concise version if you are into that sort of thing:
+// export const qs = (selector, parent = document) => parent.querySelector(selector);
+
+// retrieve data from localstorage
+export function getLocalStorage(key) {
+  return JSON.parse(localStorage.getItem(key));
 }
-export function setLocalStorage(e, t) {
-  localStorage.setItem(e, JSON.stringify(t));
+// save data to local storage
+export function setLocalStorage(key, data) {
+  localStorage.setItem(key, JSON.stringify(data));
 }
-export function setClick(e, t) {
-  qs(e).addEventListener("touchend", (n) => {
-    n.preventDefault(), t();
-  }),
-    qs(e).addEventListener("click", t);
+// set a listener for both touchend and click
+export function setClick(selector, callback) {
+  qs(selector).addEventListener("touchend", (event) => {
+    event.preventDefault();
+    callback();
+  });
+  qs(selector).addEventListener("click", callback);
+}
+
+export function getParam(param) {
+  const querystring = window.location.search;
+  const urlParams = new URLSearchParams(querystring);
+  const product = urlParams.get(param);
+
+  return product;
+}
+
+export function renderListWithTemplate(
+  template,
+  parentElement,
+  list,
+  callback
+) {
+  list.forEach((product) => {
+    const clone = template.content.cloneNode(true);
+    const hydratedTemplate = callback(clone, product);
+    parentElement.appendChild(hydratedTemplate);
+  });
 }
