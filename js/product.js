231b509e06f3e0c268dc8da2d80f60329ac4f24f
arrowalker3 <arrowalker3@gmail.com>
2022-01-28 10:57:54 -0700

    Updates

--- js/product.js
+++ js/product.js
@@ -1,1 +1,27 @@
-let products=[];function convertToJson(t){if(t.ok)return t.json();throw new Error("Bad Response")}function setLocalStorage(t,o){if(localStorage.getItem("so-cart")==null){let e=[];e.push(o),localStorage.setItem(t,JSON.stringify(e))}else{let e=localStorage.getItem("so-cart");e=JSON.parse(e),e.push(o),localStorage.setItem(t,JSON.stringify(e))}}function getProductsData(){fetch("../json/tents.json").then(convertToJson).then(t=>{products=t})}function addToCart(t){const o=products.find(e=>e.Id===t.target.dataset.id);setLocalStorage("so-cart",o)}getProductsData(),document.getElementById("addToCart").addEventListener("click",addToCart);
+let products = [];
+function convertToJson(t) {
+  if (t.ok) return t.json();
+  throw new Error("Bad Response");
+}
+function setLocalStorage(t, o) {
+  if (localStorage.getItem("so-cart") == null) {
+    let e = [];
+    e.push(o), localStorage.setItem(t, JSON.stringify(e));
+  } else {
+    let e = localStorage.getItem("so-cart");
+    (e = JSON.parse(e)), e.push(o), localStorage.setItem(t, JSON.stringify(e));
+  }
+}
+function getProductsData() {
+  fetch("../json/tents.json")
+    .then(convertToJson)
+    .then((t) => {
+      products = t;
+    });
+}
+function addToCart(t) {
+  const o = products.find((e) => e.Id === t.target.dataset.id);
+  setLocalStorage("so-cart", o);
+}
+getProductsData(),
+  document.getElementById("addToCart").addEventListener("click", addToCart);
